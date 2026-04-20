02d32c5ad1e314a162818512ee8fb83d741453bb
Ariful Haque <ariful.haque@digital.homeoffice.gov.uk>
2026-04-20 10:35:02 +0100

    feat(iam.tf): update github odic rule to use multiple repo for sub

--- variables.tf
+++ variables.tf
@@ -1,27 +1,27 @@
 variable "waf_acl_id" {
-  type = string
+  type        = string
   description = "ARN of the waf to be attached to the cloudfront."
 }
 
 variable "tenant_vars" {
   type = object({
     # enforced tags, all required attributes for costing and ID
-    cost_centre                     = string
-    account_code                    = string
-    portfolio_id                    = string
-    project_id                      = string
-    service_id                      = string
+    cost_centre  = string
+    account_code = string
+    portfolio_id = string
+    project_id   = string
+    service_id   = string
 
     # other attributes, required for cloudfront creation
-    repository                      = string
-    github_environment_name         = string
-    cloudfront_aliases              = list(string)
-    cloudfront_cert                 = string
+    repositories            = list(string)
+    github_environment_name = string
+    cloudfront_aliases      = list(string)
+    cloudfront_cert         = string
 
     #required for naming of resources
     #e.g. "cc-static-site-${var.tenant_vars.product}-${var.tenant_vars.component}"
-    component                       = string
-    product                         = string
+    component = string
+    product   = string
   })
 }
 
